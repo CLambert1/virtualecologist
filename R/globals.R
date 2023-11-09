6e3a8bb4642b008d25bb14d6d8b223b9727f8fda
CLambert1 <charlotte.anne.lambert@gmail.com>
2023-11-09 15:57:12 +0100

    Add globals.R

---
+++ R/globals.R
@@ -0,0 +1,14 @@
+globalVariables(unique(c(
+  # Merge:
+  "geometry",
+  # generate_survey_plan : <anonymous>:
+  ".",
+  # launch_survey_on_movement:
+  "centroid_x", "centroid_y", "detected", "seg_id",
+  # launch_survey_on_movement : <anonymous>:
+  "dist_seg_edge", "overlap_duration", "seg_id", "track_id",
+  # simulate_trajectory_CPF:
+  "dist_col", "env", "Lat", "Lon",
+  # simulate_trajectory_FR:
+  "env", "Lat", "Lon"
+)))
